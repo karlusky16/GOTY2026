741e58e0d727de02aabd32ea7ca46a9eef62aebc
JFG16b <Javierferrerga16@gmail.com>
2025-10-10 16:53:26 +0200

    tablero

---
+++ GOTY2026/Assets/Scripts/Tile.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    [SerializeField] private Color mainColor, otherColor;
+    private SpriteRenderer render;
+    public GameObject _highlight;
+
+    public void Init(bool isOffset)
+    {
+        Debug.Log(mainColor);
+        render = GetComponent<SpriteRenderer>();
+        if (isOffset)
+        { 
+            render.color = Color.black;
+        }
+            
+        else
+        {
+            render.color = Color.red;
+        }
+    }
+    
+    void OnMouseEnter()
+    {
+
+        _highlight.SetActive(true);
+    }
+
+    void OnMouseExit()
+    {
+        _highlight.SetActive(false);
+    }
+}
