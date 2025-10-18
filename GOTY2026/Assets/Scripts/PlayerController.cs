902a920024f8fdf4e9644411cd963159814e2939
AngelPM <mendezprietoangel@gmail.com>
2025-10-18 22:27:12 +0200

    restablecer2

--- GOTY2026/Assets/Scripts/PlayerController.cs
+++ GOTY2026/Assets/Scripts/PlayerController.cs
@@ -1,27 +1,10 @@
-using System;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public Action<int> JugadorRecibeDaño;
-    [SerializeField] private int vidaMaxima;
-    [SerializeField] private int vidaActual;
-
-    [SerializeField] private int energiaMaxima;
-    [SerializeField] private int energiaActual;
-
-    [SerializeField] private int manaMaxima;
-    [SerializeField] private int manaActual;
-
-
-
-    private void Awake()
-    {
-        vidaActual = vidaMaxima;
-        energiaActual = energiaMaxima;
-        manaActual = manaMaxima;
-    }
-
+    public int vidaPlayer = 10;
+    public int manaPlayer = 4;
+    public int energiaPlayer = 4;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -31,41 +14,21 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
-    //Getters
-    public int GetVidaMaxima() => vidaMaxima;
-    public int GetVidaActual() => vidaActual;
-
-    //Modificar vida del jugador
     public void ReducirVida(int vida)
     {
-        if ((vidaActual -= vida) < 0) vidaActual = 0;
-        JugadorRecibeDaño?.Invoke(vidaActual);
-    }
-    public void AumentarVida(int vida)
-    {
-        if ((vidaActual += vida) > vidaMaxima) vidaActual = vidaMaxima;
+        if ((vidaPlayer -= vida) < 0) vidaPlayer = 0;
     }
 
-    //Modificar energia del jugador
     public void ReducirEnergia(int energia)
     {
-        if ((energiaActual -= energia) < 0) energiaActual = 0;
+        if ((energiaPlayer -= energia) < 0) energiaPlayer = 0;
     }
-    public void AumentarEnergia(int energia)
+    
+     public void ReducirMana(int mana)
     {
-        if ((energiaActual += energia) > energiaMaxima) energiaActual = energiaMaxima;
-    }
-
-    //Modificar mana del jugador
-    public void ReducirMana(int mana)
-    {
-        if ((manaActual -= mana) < 0) manaActual = 0;
-    }
-    public void AumentarMana(int mana)
-    {
-        if ((manaActual += mana) > manaMaxima) manaActual = manaMaxima;
+        if ((manaPlayer -= mana) < 0) manaPlayer = 0;
     }
 }
--- GOTY2026/Assets/Scripts/Tile.cs
+++ GOTY2026/Assets/Scripts/Tile.cs
@@ -1,51 +1,57 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
+<<<<<<< Updated upstream
+=======
 using System.Linq;
 using JetBrains.Annotations;
+using TMPro;
 using Unity.VisualScripting;
+>>>>>>> Stashed changes
 using UnityEngine;
 
 public class Tile : MonoBehaviour
 {
     [SerializeField] private Color mainColor, otherColor;
     private SpriteRenderer render;
-    private String lastPatron;
     public GameObject _highlight;
-    public GameObject gridManager;
-    public Boolean ocupado = false;
-    public GameObject ocupadoObj;
-    //Variables para obtener la posicion del tile
-    public int x, y;
 
-    void Start() {
-    }
     public void Init(bool isOffset)
     {
         Debug.Log(mainColor);
         render = GetComponent<SpriteRenderer>();
         if (isOffset)
-        {
+        { 
             render.color = Color.black;
         }
-
+            
         else
         {
             render.color = Color.red;
         }
     }
-
+    
     void OnMouseEnter()
     {
 
+<<<<<<< Updated upstream
         _highlight.SetActive(true);
     }
 
-            Vector2[] direcciones ={new Vector2(-1, 0),new Vector2(1, 0),new Vector2(0, -1),new Vector2(0, 1)};
-            foreach (var dir in direcciones)
+    void OnMouseExit()
+    {
+=======
+        if (Player.cartaSeleccionada == true)
+        {
+            if (Player.carta.GetComponent<DisplayCard>().patron == "Cruz")
             {
-                if (GridManager._tiles.TryGetValue(new Vector2(x, y) + dir, out Tile tile))
-                    tile.Highlight();
+                lastPatron = "Cruz";
+                Highlight();
+
+                Vector2[] direcciones = { new Vector2(-1, 0), new Vector2(1, 0), new Vector2(0, -1), new Vector2(0, 1) };
+                foreach (var dir in direcciones)
+                {
+                    if (GridManager._tiles.TryGetValue(new Vector2(x, y) + dir, out Tile tile))
+                        tile.Highlight();
                 }
             }
         }
@@ -53,6 +59,20 @@ public class Tile : MonoBehaviour
     }
     void OnMouseExit()
     {
+        if (Player.cartaSeleccionada == true)
+        {
+            if (Player.carta.GetComponent<DisplayCard>().patron == "Cruz")
+            {
+                UnHighlight();
+
+                Vector2[] direcciones = { new Vector2(-1, 0), new Vector2(1, 0), new Vector2(0, -1), new Vector2(0, 1) };
+                foreach (var dir in direcciones)
+                {
+                    if (GridManager._tiles.TryGetValue(new Vector2(x, y) + dir, out Tile tile))
+                        tile.UnHighlight();
+                }
+            }
+        }
         else
         {
             UnHighlight();
@@ -98,7 +118,7 @@ public class Tile : MonoBehaviour
     }
 
     void UnHighlight() {
-
+>>>>>>> Stashed changes
         _highlight.SetActive(false);
     }
 
