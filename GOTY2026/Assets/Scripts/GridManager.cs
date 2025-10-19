72586e714d2a1e6be79c36047cb533053ebdfc90
carla de lucas <karlusky16@gmail.com>
2025-10-19 18:44:02 +0200

    .

--- GOTY2026/Assets/Scripts/GridManager.cs
+++ GOTY2026/Assets/Scripts/GridManager.cs
@@ -40,7 +40,7 @@ public class GridManager : MonoBehaviour
                 spawnedTile.y = y;
             }
         }
-        _cam.transform.position = new Vector3((float)_width / 2 - 0.5f, (float)_height / 2 - 0.5f, -10);
+        _cam.transform.position = new Vector3((float)_width / 2 - 0.5f, (float)_height / 3 - 0.5f, -10);
     }
 
     void Update()
