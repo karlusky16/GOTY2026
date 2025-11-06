dfce6b3569c4ddbe62902c7e4ef667e1b4915695
alvaroogg05 <alvarogarciaru@gmail.com>
2025-11-07 00:33:55 +0100

    Obstaculos
    He creado los obstáculos como scriptableObjects, ahora mismo solo hay un único obstáculo (piedra)

--- GOTY2026/Assets/Scripts/GameManager.cs
+++ GOTY2026/Assets/Scripts/GameManager.cs
@@ -2,12 +2,15 @@ using System;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.AI;
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
     public GameObject prefabEnemigo;
     public GameObject prefabPlayer;
     public GameObject prefabCarta;
+
+    public GameObject prefabObstaculo;
     // UI
     public GameObject deathScreen;
     public GameObject victoryScreen;
@@ -16,14 +19,22 @@ public class GameManager : MonoBehaviour
     public static GameObject carta;
     public static GameObject player;
     public static Dictionary<GameObject, Vector2> enemigos = new();
+    public static Dictionary<GameObject, Vector2> obstacles = new();
     public static List<GameObject> enemigosLis = new();
-    public static GameObject enemy; // De momento solo hay un enemigo
+    public static List<GameObject> obstaclesLis = new();
+    public static GameObject enemy;
+    public static GameObject obstacle;
     public static List<Card> cardList;
     public static List<Enemy> enemyList;
+
+    public static List<Obstacle> obstacleList;
+
+    
     void Start()
     {
         cardList = new List<Card>(Resources.LoadAll<Card>("Cartas"));
         enemyList = new List<Enemy>(Resources.LoadAll<Enemy>("Enemigos"));
+        obstacleList = new List<Obstacle>(Resources.LoadAll<Obstacle>("Obstacles"));
         // Si la instancia no existe, crea una y marca el objeto para no ser destruido.
         if (instance == null)
         {
@@ -36,6 +47,7 @@ public class GameManager : MonoBehaviour
         //añade enemigo al array de enemigos.
         GenerarEnemigos();
         TurnManager.playerController = player.GetComponent<PlayerController>();
+        GenerarObstaculos();
 
 
     }
@@ -48,6 +60,22 @@ public class GameManager : MonoBehaviour
         enemigosLis[enemigosLis.Count - 1].GetComponent<DisplayEnemy>().ActualizarID(id);
         GridManager._tiles[pos].ocupadoObj = enemigosLis[enemigosLis.Count - 1];
         GridManager._tiles[pos].ocupado = true;
+    }
+
+    public void InstanciateObstacle(Vector2 pos, int id)
+    {
+        if (GridManager._tiles[pos].ocupadoObj == null)
+        {
+            obstaclesLis.Add(Instantiate(prefabObstaculo, new(GridManager._tiles[pos].transform.position.x, GridManager._tiles[pos].transform.position.y, -0.01f), Quaternion.identity));
+            obstacles.Add(obstaclesLis[obstaclesLis.Count - 1], pos);
+            obstaclesLis[obstaclesLis.Count - 1].GetComponent<DisplayObstacle>().ActualizarID(id);
+            GridManager._tiles[pos].ocupadoObj = prefabObstaculo;
+            GridManager._tiles[pos].ocupado = true;
+        }
+        else
+        {
+            Debug.LogError(" La casilla donde se ha intentado instanciar el obstaculo no estaba vacía");
+        }
     }
 
     public void TilesEnemigos()
@@ -89,7 +117,14 @@ public class GameManager : MonoBehaviour
         InstanciateEnemy(new Vector2(4, 4), 1);
         InstanciateEnemy(new Vector2(2, 4), 1);
         InstanciateEnemy(new Vector2(2, 2), 1);
-        InstanciateEnemy(new Vector2(2, 1), 1); 
+        InstanciateEnemy(new Vector2(2, 1), 1);
+    }
+    
+    public void GenerarObstaculos()
+    {
+        InstanciateObstacle(new Vector2(2, 3), 1);
+        InstanciateObstacle(new Vector2(4, 3), 1);
+
     }
     public void Salir()
     {
