f42315b7659799105978b736e74788369d54c564
AngelPM <mendezprietoangel@gmail.com>
2025-11-08 15:35:17 +0100

    Mapa

--- GOTY2026/Assets/Scripts/DisplayCard.cs
+++ GOTY2026/Assets/Scripts/DisplayCard.cs
@@ -21,6 +21,7 @@ public class DisplayCard : MonoBehaviour
     public void ActualizarID(int nuevoDisplayID)
     {
         displayID = nuevoDisplayID;
+<<<<<<< Updated upstream
         card = CardDataBase.cardList[displayID - 1];
         this.id = card.id;
         this._name = card._name;
@@ -29,6 +30,33 @@ public class DisplayCard : MonoBehaviour
         nameText.text = " " + _name;
         costText.text = " " + coste;
         descriptionText.text = " " + descripcion;
+=======
+        Debug.Log("ActualizarID llamado con ID: " + (displayID - 1));
+        Debug.Log("CardList Count: " + GameManager.cardList.Count);
+        card = GameManager.cardList[displayID - 1];
+        nameText.text = " " + card._name;
+        costText.text = " " + card.coste;
+        DañoText.text = "Daño " + card.daño + "\n" + "Rango " + card.rango + "\n";
+        descriptionText.text = " " + card.descripcion;
+        cardImage.sprite = card.sprite;
+        Color32 mana = new(95, 16, 120, 255);
+        Color32 energia = new(29, 115, 0,255);
+        if (GetTipoCoste() == 0)
+        {
+            borde.color = mana;
+            _color = mana;
+        }
+        else if (GetTipoCoste() == 1)
+        {
+            borde.color = energia;
+            _color = energia;
+        }
+        else if (GetTipoCoste() == 2)
+        {
+            borde.color =  Color.Lerp(mana, energia, 0.5f);
+            _color = borde.color;
+        }
+>>>>>>> Stashed changes
     }
     public Card GetCard() => card;
     public int GetTipo() => card.tipo;
