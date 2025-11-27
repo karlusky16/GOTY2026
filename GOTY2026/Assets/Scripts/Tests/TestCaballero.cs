ba3f077fe43bf5521b84057f32c9dbc4d63338ed
AngelPM <mendezprietoangel@gmail.com>
2025-11-27 13:51:01 +0100

    Cartas

---
+++ GOTY2026/Assets/Scripts/Tests/TestCaballero.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+public class TestCaballero
+{
+    // Prepara GridManager, GameManager.enemyList y GameObjects necesarios para que el test funcione
+    [Test]
+    public void TestCaballeroSimplePasses()
+    {
+
+    }
+}
